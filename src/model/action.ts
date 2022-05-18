39ef2f5957250cf4bf41a057fd1d4d67dece45e1
Charles Murphy <charles.murphy.042@gmail.com>
2022-05-18 10:00:54 -0400

    Update action.ts

--- src/model/action.ts
+++ src/model/action.ts
@@ -36,7 +36,7 @@ const Action = {
   checkCompatibility() {
     const currentPlatform = process.platform;
     if (!Action.supportedPlatforms.includes(currentPlatform)) {
-      throw new Error(`Currently ${currentPlatform}-platform is not supported`);
+      throw new Error(`Currently ${currentPlatform}-platform is not supported on this fork`);
     }
   },
 };
